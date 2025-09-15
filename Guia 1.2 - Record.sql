25b7286e79a86444307c43ba3cc83227f74ec730
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-09-14 22:37:41 -0300

    Record exercise fixed

--- Guia 1.2 - Record.sql
+++ Guia 1.2 - Record.sql
@@ -1,26 +1,23 @@
 DECLARE
 
-    TYPE SalaryArray IS VARRAY(37) OF NUMBER;
-    
-    v_Salary_Array SalaryArray; 
-    v_sum NUMBER := 0 ;
-    v_promedio NUMBER := 0;
+TYPE DeptRec IS RECORD (
+    deptno departments.department_id%TYPE,
+    dname departments.department_name%TYPE,
+    loc locations.city%TYPE
+);
+
+v_michi DeptRec;
 
 BEGIN
+    
+    SELECT department_id, department_name, city INTO v_michi
+    FROM departments d
+    JOIN locations l
+    ON d.location_id = l.location_id
+    WHERE department_id = 10;
 
-    v_Salary_Array := SalaryArray(2500, 2600, 2800, 2900, 3100, 4200, 4400, 4800);
-    
-    SELECT SALARY 
-        BULK COLLECT INTO v_Salary_Array
-    FROM EMPLOYEES;
-    
-    FOR i IN 1..  v_Salary_Array.COUNT LOOP
-        v_sum := v_sum + v_Salary_Array(i);
-    END LOOP;
-    
-    v_promedio := v_sum / v_Salary_Array.COUNT;
-    
-    
-    DBMS_OUTPUT.PUT_LINE('Promedio final: ' || ROUND(v_promedio, 2));
-    
+ DBMS_OUTPUT.PUT_LINE('Número departamento: ' || v_michi.deptno || 
+                        ', Nombre departamento: ' || v_michi.dname || 
+                        ', Ciudad: ' || v_michi.loc);
+
 END;
